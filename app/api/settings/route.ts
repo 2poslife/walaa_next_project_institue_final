f8236064e096bb77f9c727665c4b48a735c451aa
2poslife <2poslife@gmail.com>
2026-04-18 14:23:23 +0300

    fix

--- app/api/settings/route.ts
+++ app/api/settings/route.ts
@@ -7,6 +7,8 @@ import {
   unauthorizedResponse,
 } from '@/lib/utils/api-response';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: NextRequest) {
   try {
     const user = getUserFromRequest(request);
